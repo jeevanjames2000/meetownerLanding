fd73a6d2bd051d702cc36ca0c002a275d3dc089a
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-04-16 14:51:13 +0530

    sd

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     strictPort: true,
     cors: true,
     hmr: {
-      host: "preprod.meetowner.in",
+      host: "https://preprod.meetowner.in/",
     },
     base: "/",
   },
