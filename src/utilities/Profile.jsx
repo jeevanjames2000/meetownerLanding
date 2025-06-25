0c8fb7c2a791b5fe83b12d12de7396c65588c580
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-06-25 18:22:07 +0530

    sd

--- src/utilities/Profile.jsx
+++ src/utilities/Profile.jsx
@@ -206,7 +206,7 @@ export default function ProfilePage() {
               placeholder="Mobile"
               className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               aria-label="Mobile"
-              disabled={loading}
+              disabled
             />
             <input
               type="password"
